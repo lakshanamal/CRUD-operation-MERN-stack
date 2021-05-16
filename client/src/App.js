ba81086ea5a0e178cab2fded1197bb2ea698078d
lakshanamal <lakshanamal100@gmail.com>
2021-05-17 01:54:26 +0530

    insert crud is done

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import Axios from 'axios';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
+  const [foodName, setFoodName] = useState("");
+  const [days, setDays] = useState(0);
+
+  const addToDo=()=>{
+    console.log(foodName+days);
+    Axios.post('http://localhost:3001/add',{foodName:foodName,days:days})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD app with MERN</h1>
+        <div className="mb-3">
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Food Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => {
+              setFoodName(e.target.value);
+            }}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="exampleInputPassword1" className="form-label">
+            Days Since you Ate It :
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            onChange={(e) => {
+              setDays(e.target.value);
+            }}
+          />
+        </div>
+        <button type="submit" className="btn btn-primary" onClick={addToDo}>
+          Submit
+        </button>
+      
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,26 +1,35 @@
 const express=require('express');
 const mongoose=require('mongoose');
+const cors =require('cors');
+const app=express();
 
 // import model food before using this
 const FoodModel=require('./model/Food');
+
+app.use(express.json());
+app.use(cors()); 
 
 mongoose.connect('mongodb://localhost:27017/CRUD',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-const app=express();
-
-app.use(express.json());
-
 // api 
 
-app.get('/',(req,res)=>{
+app.post('/add',(req,res)=>{
+
+    const foodName=req.body.foodName;
+    const days=req.body.days;
     const food=new FoodModel({
-        foodName:"Apple",
-        daysSinceIAte:3
+        foodName:foodName,
+        daysSinceIAte:days
     })   /// create new food item 
     food.save().then(()=>res.json("Sucessfull added...")).catch(err=>res.status(400).json('Error :'+err))
+    
+})
+app.get('/',(req,res)=>{
+       
+    FoodModel.find().then((result)=>res.json(result)).catch(err=>res.status(400).json('Error :'+err))
     
 })
 
