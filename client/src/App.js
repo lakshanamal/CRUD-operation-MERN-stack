5cf131c57cb217cc590180a5aca390fea2d4e1b9
lakshanamal <lakshanamal100@gmail.com>
2021-05-17 09:09:42 +0530

    get request done

--- client/src/App.js
+++ client/src/App.js
@@ -1,46 +1,72 @@
-import React, { useState } from "react";
-import Axios from 'axios';
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   const [foodName, setFoodName] = useState("");
   const [days, setDays] = useState(0);
+  const [foodList, setFoodList] = useState([]);
 
-  const addToDo=()=>{
-    console.log(foodName+days);
-    Axios.post('http://localhost:3001/add',{foodName:foodName,days:days})
-  }
+  useEffect(() => {
+    Axios.get("http://localhost:3001").then((res) => setFoodList(res.data));
+  }, []);
+  const addToDo = () => {
+    console.log(foodName + days);
+    Axios.post("http://localhost:3001/add", { foodName: foodName, days: days });
+    setFoodName("");
+    setDays(0);
+  };
   return (
     <div className="App">
       <h1>CRUD app with MERN</h1>
-        <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">
-            Food Name
-          </label>
-          <input
-            type="text"
-            className="form-control"
-            onChange={(e) => {
-              setFoodName(e.target.value);
-            }}
-          />
-        </div>
-        <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">
-            Days Since you Ate It :
-          </label>
-          <input
-            type="number"
-            className="form-control"
-            onChange={(e) => {
-              setDays(e.target.value);
-            }}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary" onClick={addToDo}>
-          Submit
-        </button>
-      
+      <div className="mb-3">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Food Name
+        </label>
+        <input
+          type="text"
+          className="form-control"
+          onChange={(e) => {
+            setFoodName(e.target.value);
+          }}
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Days Since you Ate It :
+        </label>
+        <input
+          type="number"
+          className="form-control"
+          onChange={(e) => {
+            setDays(e.target.value);
+          }}
+        />
+      </div>
+      <button type="submit" className="btn btn-primary" onClick={addToDo}>
+        Submit
+      </button>
+      <h1>Food List</h1>
+
+      {foodList.map((val, key) => {
+        return (
+          <div key={key} className="card text-center mb-3">
+            <div className="card-body">
+              <h5 className="card-title">{val.foodName}</h5>
+              <div>
+                <input type="text" />
+                <a href="#" className="btn btn-primary ml-2">
+                  Update
+                </a>
+              </div>
+              <a href="#" className="btn btn-primary">
+                Delete
+              </a>
+            </div>
+            <div className="card-footer text-muted">{val.daysSinceIAte}</div>
+          </div>
+        );
+      })}
     </div>
   );
 }
